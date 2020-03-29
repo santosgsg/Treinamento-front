534d42caad84d4270028870235f883f1fdccd102
Santos <gabrielsantos.s.guedes@gmail.com>
2020-03-29 02:25:58 -0300

    inicio cadastro de endereco

--- src/app/spas/pessoas/pessoa-incluir-alterar.controller.js
+++ src/app/spas/pessoas/pessoa-incluir-alterar.controller.js
@@ -50,6 +50,8 @@ function PessoaIncluirAlterarController(
         vm.tituloTela = "Cadastrar Pessoa";
         vm.acao = "Cadastrar";
 
+        vm.tituloModal = "Cadastrar Endereço"
+        vm.acaoModal = "Cadastrar"
 
         /**Recuperar a lista de perfil */
         vm.listar(vm.urlPerfil).then(
@@ -242,36 +244,6 @@ function PessoaIncluirAlterarController(
         var dia = data.slice(8, 10);
 
         return dia + mes + ano;
-    };
-
-    vm.listaUF = [
-        { "id": "RO", "desc": "RO" },
-        { "id": "AC", "desc": "AC" },
-        { "id": "AM", "desc": "AM" },
-        { "id": "RR", "desc": "RR" },
-        { "id": "PA", "desc": "PA" },
-        { "id": "AP", "desc": "AP" },
-        { "id": "TO", "desc": "TO" },
-        { "id": "MA", "desc": "MA" },
-        { "id": "PI", "desc": "PI" },
-        { "id": "CE", "desc": "CE" },
-        { "id": "RN", "desc": "RN" },
-        { "id": "PB", "desc": "PB" },
-        { "id": "PE", "desc": "PE" },
-        { "id": "AL", "desc": "AL" },
-        { "id": "SE", "desc": "SE" },
-        { "id": "BA", "desc": "BA" },
-        { "id": "MG", "desc": "MG" },
-        { "id": "ES", "desc": "ES" },
-        { "id": "RJ", "desc": "RJ" },
-        { "id": "SP", "desc": "SP" },
-        { "id": "PR", "desc": "PR" },
-        { "id": "SC", "desc": "SC" },
-        { "id": "RS", "desc": "RS" },
-        { "id": "MS", "desc": "MS" },
-        { "id": "MT", "desc": "MT" },
-        { "id": "GO", "desc": "GO" },
-        { "id": "DF", "desc": "DF" }
-    ];
+    }
 
 }
